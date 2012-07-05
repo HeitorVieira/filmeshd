dddda2bf23175bf07d0483f0a05c7001445d8c89
Heitor Vieira <halbuquerquevieira@gmail.com>
2012-07-05 13:16:36 -0300

    criacao do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filmeshd::Application.config.secret_token = 'e229c775256aca5cde51bbb3aea6196568ac18ed9ece4d1252405cd9320683511c02ae764d517bcff24589e1ed0a90e4d63e473081e17cd68b44d47ffb9330fc'
